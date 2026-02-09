8097f75e0c41f39b42b641824f2e878d05c48c3d
juliotwo1996@gmail.com <juliotwo1996@gmail.com>
2026-02-09 16:15:05 -0600

    vercel

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -118,7 +118,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
-        conn_max_age=600
+        conn_max_age=600,
+        ssl_require='VERCEL' in os.environ  # Force SSL on Vercel
     )
 }
 
